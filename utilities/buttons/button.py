cd2d45a5ee1986351e13e7112d0e401b8d63ebe7
nypava <ny.dev0.em@gmail.com>
2024-08-21 14:35:58 +0300

    chore: remove source code button

--- utilities/buttons/button.py
+++ utilities/buttons/button.py
@@ -4,8 +4,5 @@ start_btn = InlineKeyboardMarkup([
         InlineKeyboardButton("Channel",url="https://t.me/neuralp"),
         InlineKeyboardButton("Group",url="https://t.me/neuralg"),
     ],
-    [
-        InlineKeyboardButton("📁 Source code",url="https://github.com/NYDEV0/SpeechRecognitionBot"),
-    ]
 ])
 
